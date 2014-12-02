dfae4fc3398e8ccdd829f6187fc3588a20df1265
sebseb7 <github@seb.exse.net>
2014-12-02 12:40:24 +0100

    fprint -> swo debug (https://github.com/karlp/swopy)

---
+++ core/syscalls.c
@@ -0,0 +1,84 @@
+// from newlib_stubs.c
+
+#if STM32F == 2
+#include "stm32f2xx.h"
+#endif
+#if STM32F == 4
+#include "stm32f4xx.h"
+#endif
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/unistd.h>
+
+caddr_t _sbrk(int incr) {
+
+	extern char _ebss; // Defined by the linker
+	static char *heap_end;
+	char *prev_heap_end;
+
+	if (heap_end == 0) {
+		heap_end = &_ebss;
+	}
+	prev_heap_end = heap_end;
+
+	//todo: check consumtion
+	//char * stack = (char*) __get_MSP();
+	if (heap_end + incr >  (char*)0x20020000)
+	{
+		//_write (STDERR_FILENO, "Heap and stack collision\n", 25);
+		errno = ENOMEM;
+		return  (caddr_t) -1;
+		//abort ();
+	}
+
+	heap_end += incr;
+	return (caddr_t) prev_heap_end;
+
+}
+
+int _write(int file, char *ptr, int len)
+{
+	int i=0;
+	for(i=0 ; i<len ; i++)
+		ITM_SendChar((*ptr++));
+	return len;
+}
+
+int _fstat(int file, struct stat *st)
+{
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _isatty(int file)
+{
+
+
+	switch (file)
+	{
+		case STDOUT_FILENO:
+		case STDERR_FILENO:
+		case STDIN_FILENO:
+			return 1;
+		default:
+			errno = EBADF;
+			return 0;
+	}
+}
+
+int _close(int file)
+{
+	return -1;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+	return 0;
+}
+
+int _read(int file, char *ptr, int len)
+{
+	errno = EBADF;
+	return -1;
+}
